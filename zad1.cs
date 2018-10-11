cd19a34e23a0f80683b4e0bdc190a1b7bebcfd33
nikola649 <nikola649@gmail.com>
2018-10-11 20:54:37 +0200

    za pocetak

---
+++ zad1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class Command
+    {
+        public string Help()
+        {
+        }
+        public void Add()
+            {
+            }
+
+        public void Remove()
+            {}
+        public void Display()
+        { }
+        public List List()
+        { }
+       
+}
+    class Role
+    {
+    }
+    class covjek
+    { 
+       //ovo je roditelj klasa za sve sto je isto
+        private string FirstName;
+        private string LastName;
+        private int Age;
+        
+    }
+}
